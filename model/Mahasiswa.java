4de1798e5febb185e4b39305783f534190f1a9d2
FerdyPriamjaya <muhammadferdy323@gmail.com>
2024-05-30 17:56:10 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilaninfo(){
+  System.out.println();
+  System.out.println("=============Data Mahasiswa===========");
+  System.out.println("NIDN : 2210010575");
+  System.out.println("Nama : Muhammad Ferdy Priamjaya");
+  System.out.println("======================================");
+ }
+}
